7424cc94a7c1b55992cf9e30a8a1fde61230bff1
Jacob Roth <jakeroth0@gmail.com>
2023-03-24 14:01:20 -0400

    setup project structure and dependencies

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const routes = require('./routes');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+mongoose.connection.once('connected', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// Placeholder route to avoid the error
+router.get('/', (req, res) => {
+  res.send('Thought routes');
+});
+
+module.exports = router;
